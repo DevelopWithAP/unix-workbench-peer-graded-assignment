978b83b2d2ac811680f8dd5c8686865eb02cb05c
Apostolos Papas <apostolisparga@gmail.com>
2021-11-28 18:17:07 +0000

    First part of assignment

---
+++ README.md
@@ -0,0 +1,3 @@
+Title: Guessing Game
+Generated on: Sun 28 Nov 2021 18:09:24 GMT
+The script contains 22 lines
---
+++ guessinggame.sh
@@ -0,0 +1,22 @@
+num_files=$(ls . | wc -l)
+function guess {
+	while true
+	do
+		read guess
+		if [[ $guess -gt $num_files ]]
+		then
+                        sleep 1
+			echo "You guessed too high. Try again"
+		elif [[ $guess -lt $num_files ]]
+		then
+                        sleep 1
+			echo "You guessed too low. Try again"
+		else
+                        sleep 1
+			echo "Congratulations! There are indeed $guess files in this directory"
+		break
+		fi
+	done 
+}
+echo "How many files in this directory?"
+guess
